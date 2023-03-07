493290a7d4a9ab1a037fa02a90925ae66483b831
ekimkael <michelmbili@gmail.com>
2023-03-07 06:37:41 +0100

    feat(app): fresh install Next 13 for new version of the folio

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-const path = require("path")
-
-module.exports = {
-	sassOptions: {
-		includePaths: [path.join(__dirname, "styles")],
-	},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
--- templates/auth.template.js
+++
@@ -1,5 +0,0 @@
-function AuthTemplate({ children }) {
-	return <div className="fullscreen flex-centered">{children}</div>
-}
-
-export default AuthTemplate
